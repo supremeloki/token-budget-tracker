76e6161a6a2c76d64a7219fff47cdca77bcd6e13
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-02-21 09:32:28 +0330

    fix off-by-one in chunker pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -102,3 +102,4 @@ def test_report_utilization_grows():
 def test_can_afford_checks_category():
     tracker = TokenBudgetTracker(total_limit=500)
     assert tracker.can_afford("response", 100)
+    assert not tracker.can_afford("response", 10_000)
